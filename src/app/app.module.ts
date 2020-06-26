73936602ea46e4ace35f9fceb3e7227999d935b9
Valentina Orejuela <valentina.orejuela_a@uao.edu.co>
2020-06-26 04:08:54 -0500

    Se realizo en encabezado con el menu correspondiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,15 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms'
 import { NgxSpinnerModule } from "ngx-spinner";
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +28,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
     BrowserAnimationsModule,
     ReactiveFormsModule,
     NgxSpinnerModule,
+    BsDropdownModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase)
 
   ],
