89d1c20bb5660f2a9d6d5ffa53b75933392cc306
Valentina Orejuela <valentina.orejuela_a@uao.edu.co>
2020-06-26 05:19:54 -0500

    Se creo la vista ver el listado clientes con unas targetas de simulación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,19 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { NgxSpinnerModule } from "ngx-spinner";
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    ListadoClientesComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +30,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     NgxSpinnerModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase)
 
